11af33963cf48190636ad4ea4386642b84bdcb00
Sanjay-Archery <sanjaymkx@gmail.com>
2026-04-07 15:01:52 +0530

    backend

--- Backend/server.js
+++ Backend/server.js
@@ -6,9 +6,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-
 app.use(cors({
-  origin: "*",
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  methods: ["GET", "POST"],
+  credentials: true
 }));
 app.use(express.json());
 
