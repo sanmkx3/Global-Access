a4f88b6d3cc345980aa063f5838f432d130ab860
Sanjay-Archery <sanjaymkx@gmail.com>
2026-04-07 14:38:36 +0530

    backend

--- Backend/server.js
+++ Backend/server.js
@@ -35,4 +35,8 @@ console.log(process.env.EMAIL_PASS);
   }
 });
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
